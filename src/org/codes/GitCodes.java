123ba4b030bd66f9e0301e1ef1643a214def2297
Gayathri <gayathrikavuvenkat01@gmail.com>
2022-11-23 21:27:07 +0530

    Git Codes

---
+++ src/org/codes/GitCodes.java
@@ -0,0 +1,19 @@
+package org.codes;
+
+public class GitCodes {
+	
+	private void git() {
+		System.out.println("Code Management tool/Software tool to store");
+
+	}
+	
+	private void gitHub() {
+		System.out.println("Cloud based server-repository");
+
+	}
+	
+	private void gitBash() {
+	System.out.println("Give unix commands in command template");
+	}
+
+}
